b1159cffeedee14859ae0c6c654c192b851dece5
erfan shafigh <erfanshafighniaaa@gmail.com>
2025-10-21 21:45:24 +0330

    Add HeroSection and LandingGallery components; enhance styles and layout

--- init-nextjs/src/app/page.tsx
+++ init-nextjs/src/app/page.tsx
@@ -1,21 +1,13 @@
-import { HeroSection } from '@/components/molecules/herosection';
+import { HeroSection } from '@/components/organisms/herosection';
 import styles from './page.module.scss';
+import LandingGallery from '@/components/organisms/landingGallery';
 
 export default function Home() {
   return (
       <div className={`${styles.home}`}>
         {/* <div className={styles.banner}></div> */}
         <HeroSection/>
-        <div className={`${styles.description} flex p-5 py-10 justify-between items-center`}>
-          <div className="title px-10 min-w-100">
-            <h5 className=" text-black font-black text-2xl">Next.js Design System</h5>
-          </div>
-          <div className="caption text-2xl leading-10">
-            <p>This is the landing page of my Next.js design system. Here I introduce the
-                components, typography, and styles that create a consistent user
-                experience across all projects.</p>
-          </div>
-        </div>
+        <LandingGallery/>
       </div>
     
   );
---
+++ init-nextjs/src/components/organisms/landingGallery.tsx
@@ -0,0 +1,15 @@
+
+
+export default function LandingGallery() {
+    return(
+        <div className="text-center p-3">
+            <div className="spacer"></div>
+            <h4 className="text-3xl font-bold">
+            Component Gallery
+            </h4>
+            <p className="text-2xl my-6 text-ring">
+                Explore our comprehensive collection of reusable components
+            </p>
+        </div>
+    );  
+}
